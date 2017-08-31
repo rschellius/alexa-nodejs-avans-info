2a2467367d95ade077e3d38ee613d42629f53aa5
Robin Schellius <r.schellius@avans.nl>
2017-08-31 19:20:34 +0200

    Added new intents and states

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var _ = require('underscore');
 
 var app = new alexa.app('coffee-skill');
 
-var launchMsg = 'Welcome to the Coffee skill.';
-var promptIntro = 'I can help you to order coffee or tea.';
-var reprompt = 'Would you like to order some coffee or tea?';
-var promptCoffeeOrTea = 'Would you like to order coffee or tea?';
+var launchMsg = 'Welcome to the barista skill.';
+var promptIntro = 'I can help you to order coffee or tea. We have several flavors available.';
+var reprompt = 'Would you like to order anything?';
+var promptCoffeeOrTea = 'What would you like?';
 var promptCoffees = 'You can order a cappuchino, a latte, an espresso, a latte machiato, or an american.';
 var promptTeas = 'You can order a mint, green tea, jasimine, or earl grey.';
 var promptMilkOrSugar = 'Would you like milk, sugar or both?';
@@ -21,6 +21,9 @@ var outputMissed = 'I didn\'t hear a what drink you\'d like.';
 var outputUnavailable = 'I\'m sorry, that drink is unavailable.';
 var outputStop = "Don't you worry. I'll be back.";
 var outputCancel = "No problem. Request cancelled.";
+var outputTypes = "We serve four types of coffee, and five types of tea.";
+var outputCoffees = "We serve regular coffee, cappuchino, espresso, latte, and americano.";
+var outputTeas = "We serve regular tea, mint, earl grey, jasmine, and gree tea.";
 
 // State management
 const INITIAL_STATE = 0;
@@ -32,7 +35,7 @@ var state = INITIAL_STATE;
 var session;
 
 var coffees = ['coffee', 'cappuchino', 'espresso', 'latte', 'americano'];
-var teas = ['green tea', 'mint', 'earl grey', 'jasmine', 'regular']
+var teas = ['tea', 'green tea', 'mint', 'earl grey', 'jasmine', 'regular']
 
 //
 // Launch intent, called at startup of this skill.
@@ -64,8 +67,6 @@ app.intent('chooseTypeOfDrink', {
             "orderedDrink": "Drinks"
         },
         "utterances": [
-            // "say the number {1-100|number}",
-            // "give me the number {1-100|number}",
             "{1-10|number} {-|orderedDrink}",
             "{1-10|number} {-|orderedDrink} please",
             "I would like {1-10|number} {-|orderedDrink}",
@@ -76,18 +77,21 @@ app.intent('chooseTypeOfDrink', {
     function(request, response) {
         // Did the user mention a number?
         var number = request.slot('number');
-        if (_.isEmpty(number)) { number = 1; };
+        if (_.isEmpty(number) || number <= 0) { number = 1; };
 
         // 
         // Depending on the state we take an action.
         //
-        console.log('chooseTypeOfDrink - current state = ' + state);
+        console.log('chooseTypeOfDrink - current state = ' + printState(state));
         switch (state) {
             case INITIAL_STATE:
 
                 // Did the user mention a drink?
                 var orderedDrink = request.slot('orderedDrink');
                 if (_.isEmpty(orderedDrink)) {
+                    // 
+                    // No drink ordered.
+                    //
                     response
                         .say(outputMissed)
                         .say(reprompt)
@@ -100,31 +104,34 @@ app.intent('chooseTypeOfDrink', {
                     // The user ordered some drink. Check if it's what we sell.
                     //
                     console.log('chooseTypeOfDrink - order = ' + number + ' ' + orderedDrink);
-                    if (!_.contains(coffees, orderedDrink) && !_.contains(teas, orderedDrink)) {
+
+                    //
+                    // Persist the type of drink in the session
+                    //
+                    if (request.hasSession()) {
+                        session = request.getSession();
+                        session.set('drink', orderedDrink);
+                        session.set('amount', number);
+                    }
+
+                    if (_.contains(coffees, orderedDrink)) {
+                        state = ORDER_COFFEE_STATE;
+                        response
+                            .say('You chose ' + number + ' ' + orderedDrink)
+                            .say(promptMilkOrSugar)
+                            .shouldEndSession(false, promptMilkOrSugar);
+                    } else if (_.contains(teas, orderedDrink)) {
+                        state = ORDER_TEA_STATE
+                        response
+                            .say('You chose ' + number + ' ' + orderedDrink)
+                            .say(promptMilkOrSugar)
+                            .shouldEndSession(false, promptMilkOrSugar);
+                    } else {
                         console.log('chooseTypeOfDrink - ' + orderedDrink + ' is not available');
                         response
                             .say(errorMsg)
-                            .shouldEndSession(false, "Do you want another number?");
-                    } else {
-
-                        // We have the correct drink. 
-                        // Adjust the state so that we can ask the next question.
-                        if (request.hasSession()) {
-                            // get the session object
-                            session = request.getSession();
-                            // set a session variable
-                            // by defailt, Alexa only persists session variables to the next request
-                            // the alexa-app module makes session variables persist across multiple requests
-                            // Note that you *must* use `.set` or `.clear` to update
-                            // session properties. Updating properties of `attributeValue`
-                            // that are objects will not persist until `.set` is called.
-                            session.set('state', ORDER_COFFEE_STATE);
-                        }
-
-                        response
-                            .say('You chose ' + number + orderedDrink)
-                            // .say("You asked for the number " + number + ". ")
-                            .shouldEndSession(false, "Do you want another number?");
+                            .say("Please order a different drink.")
+                            .shouldEndSession(false, "Would you like to make an order?");
                     }
                 };
                 break;
@@ -134,6 +141,74 @@ app.intent('chooseTypeOfDrink', {
                     .say('I am sorry, that request is invalid in this state.')
                     .shouldEndSession(false, "Do you want to order something?");
         };
+    });
+
+//
+// "What coffee or tea do you serve?"
+//
+app.intent('whatTypesOfDrink', {
+        "slots": {
+            "orderedDrink": "Drinks"
+        },
+        "utterances": [
+            "what {type|sort} of {coffee|tea} or {coffee|tea} do you {have|serve|offer|have to offer|provide}",
+            "what {type|sort} of {-|orderedDrink} do you {have|serve|offer|have to offer|provide}"
+        ]
+    },
+    function(request, response) {
+
+        var orderedDrink = request.slot('orderedDrink');
+        if (_.isEmpty(orderedDrink)) {
+            response
+                .say(outputTypes + outputCoffees + outputTeas)
+                .say(promptCoffeeOrTea)
+                .shouldEndSession(false);
+            return true;
+        } else if (orderedDrink === "coffee") {
+            response
+                .say(outputTypes + outputCoffees)
+                .say(promptCoffeeOrTea)
+                .shouldEndSession(false);
+        } else {
+            response
+                .say(outputTypes + outputTeas)
+                .say(promptCoffeeOrTea)
+                .shouldEndSession(false);
+        }
+
+    });
+
+//
+// "What coffee or tea do you serve?"
+//
+app.intent('milkOrSugar', {
+        "slots": {
+            "additions": "Additions"
+        },
+        "utterances": [
+            "{-|additions} {please |}"
+        ]
+    },
+    function(request, response) {
+
+        var additions = request.slot('additions');
+        if (_.isEmpty(additions)) {
+            response
+                .say("I'm sorry, I didn't hear your choice.")
+                .say(promptMilkOrSugar)
+                .shouldEndSession(false);
+        } else if (orderedDrink === "coffee") {
+            response
+                .say(outputTypes + outputCoffees)
+                .say(promptCoffeeOrTea)
+                .shouldEndSession(false);
+        } else {
+            response
+                .say(outputTypes + outputTeas)
+                .say(promptCoffeeOrTea)
+                .shouldEndSession(false);
+        }
+        return;
     });
 
 //
@@ -147,14 +222,13 @@ app.intent('AMAZON.YesIntent', {
     },
     function(request, response) {
 
+        var drink = "drink";
+
         if (request.hasSession()) {
             session = request.getSession();
-            console.log('YesIntent - session found. Session = ');
-            console.dir(session.details);
-
-        } else {
-            console.log('YesIntent - no session found!');
+            drink = session.get("drink");
         }
+        console.log('YesIntent - current state = ' + printState(state));
 
         // handle the yes response. Use more logic and use the session to store skill status.
         switch (state) {
@@ -164,9 +238,27 @@ app.intent('AMAZON.YesIntent', {
                     .say("What would you like to order?")
                     .shouldEndSession(false);
                 break;
+            case ORDER_COFFEE_STATE:
+                response
+                    .say("You ordered milk and sugar with your " + drink)
+                    .say("Would you like anything else?")
+                    .shouldEndSession(false);
+                break;
+            case ORDER_TEA_STATE:
+                response
+                    .say("You ordered milk and sugar with your " + drink)
+                    .say("Would you like anything else?")
+                    .shouldEndSession(false);
+                break;
+            case ORDER_MILK_SUGAR_STATE:
+                response
+                    .say("You ordered milk and sugar with your " + drink)
+                    .say("Would you like anything else?")
+                    .shouldEndSession(false);
+                break;
             default:
                 response
-                    .say('We are in default state.')
+                    .say('We are in an unknown state.')
                     .say("Just for your information: you said yes.")
                     .shouldEndSession(false);
         }
@@ -182,9 +274,11 @@ app.intent('AMAZON.NoIntent', {
         ]
     },
     function(request, response) {
+        console.log('NoIntent - current state = ' + printState(state));
+
         // handle the no response. Use more logic and use the session to store skill status.
         response
-            .say("Just for your information: you said no.")
+            .say("I heard you say no.")
             .shouldEndSession(false);
     });
 
@@ -230,5 +324,26 @@ app.intent("AMAZON.CancelIntent", {
     response.say(outputCancel);
     return;
 });
+
+var printState = function(state) {
+    var string;
+    switch (state) {
+        case INITIAL_STATE:
+            string = "INITIAL_STATE";
+            break;
+        case ORDER_COFFEE_STATE:
+            string = "ORDER_COFFEE_STATE";
+            break;
+        case ORDER_TEA_STATE:
+            string = "ORDER_TEA_STATE";
+            break;
+        case ORDER_MILK_SUGAR_STATE:
+            string = "ORDER_MILK_SUGAR_STATE";
+            break;
+        default:
+            string = "unknown state";
+    }
+    return string;
+}
 
 module.exports = app;
